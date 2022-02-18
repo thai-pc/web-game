1aacc2fef7a43ff8da6e0840a727aaff90767afc
dongthai <vodongthais1@gmail.com>
2022-02-18 10:07:12 +0700

    update filter

--- src/components/GameList/GameList.container.tsx
+++ src/components/GameList/GameList.container.tsx
@@ -1,12 +1,25 @@
-import React, { ReactElement, useEffect, useState } from 'react';
+import React, {
+  ChangeEvent,
+  ReactElement,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 import axios from 'axios';
 import GameListRender from './GameList.render';
 import { Game } from '../../types';
 import { API_HOST, API_KEY } from './constants';
+import { Filter } from './types';
 
 const GameList = (): ReactElement => {
+  const [filter, setFilter] = useState<Filter>({
+    platform: 'browser',
+    sortBy: 'relevance',
+  });
+
   const [games, setGames] = useState<Game[]>([]);
   const [err, setErr] = useState<string>('');
+
   useEffect(() => {
     axios
       .get('/games', {
@@ -23,7 +36,17 @@ const GameList = (): ReactElement => {
       .catch((err) => setErr(err.message));
   }, []);
 
-  return <GameListRender err={err} games={games} />;
+  const onFilterChange = useCallback((e: ChangeEvent<HTMLFormElement>) => {
+    setFilter((current) => ({
+      ...current,
+      [e.target.name]: e.target.value,
+    }));
+    e.preventDefault();
+  }, []);
+
+  return (
+    <GameListRender err={err} games={games} onFilterChange={onFilterChange} />
+  );
 };
 
 export default GameList;
--- src/components/GameList/GameList.render.tsx
+++ src/components/GameList/GameList.render.tsx
@@ -1,13 +1,19 @@
-import React, { ReactElement } from 'react';
+import React, { ChangeEvent, ReactElement } from 'react';
 import GameCard from '../GameCard';
 import { Game } from '../../types';
 import { List, ListItem } from './styles';
+import GameFilter from '../GameFilter';
 interface Props {
   err?: string;
   games: Game[];
+  onFilterChange: (e: ChangeEvent<HTMLFormElement>) => void;
 }
 
-const GameListRender = ({ err, games }: Props): ReactElement => {
+const GameListRender = ({
+  err,
+  games,
+  onFilterChange,
+}: Props): ReactElement => {
   if (err) {
     return <p>Unable to fetch games</p>;
   }
@@ -15,15 +21,18 @@ const GameListRender = ({ err, games }: Props): ReactElement => {
     return <p>No games available</p>;
   }
   return (
-    <List>
-      {games.map((game) => {
-        return (
-          <ListItem key={game.id}>
-            <GameCard content={game} />
-          </ListItem>
-        );
-      })}
-    </List>
+    <>
+      <GameFilter onChange={onFilterChange} />
+      <List>
+        {games.map((game) => {
+          return (
+            <ListItem key={game.id}>
+              <GameCard content={game} />
+            </ListItem>
+          );
+        })}
+      </List>
+    </>
   );
 };
 
